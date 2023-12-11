01a3dfa48d3637e19e55e2643141b80ee5758109
Luthfi Kurnia Hadi <lkurniahadi@gmail.com>
2023-12-11 14:13:36 +0700

    crud with prisma orm (services)

--- src/author/author.services.ts
+++ src/author/author.services.ts
@@ -1,3 +1,4 @@
+import { Author } from "@prisma/client";
 import {db} from "../utils/db.server";
 
 interface IAuthorResponse{
@@ -14,4 +15,58 @@ export const listAuthors =async () : Promise<IAuthorResponse[]> => {
             lastName: true
         }
     });
+}
+
+export const getAuthors =async (id: number): Promise<IAuthorResponse | null> => {
+    return db.author.findUnique({
+        where:{
+            id,
+        },
+        select:{
+            id: true,
+            firstName: true,
+            lastName: true
+        }
+    })
+}
+
+export const createAuthor =async (author:Omit<Author, "id">) : Promise<IAuthorResponse> => {
+    const {firstName, lastName} = author;
+    return db.author.create({
+        data: {
+            firstName,
+            lastName
+        }, 
+        select:{
+            id: true,
+            firstName: true,
+            lastName: true
+        }
+    })
+}
+
+export const updateAuthor =async (author:Omit<Author, "id">, id: number) : Promise<IAuthorResponse> => {
+    const {firstName, lastName} = author;
+    return db.author.update({
+        where: {
+            id
+        },
+        data:{
+            firstName,
+            lastName
+        },
+        select:{
+            id: true,
+            firstName: true,
+            lastName: true
+        }
+    })
+}
+
+export const deleteAuthor =async (id: number) : Promise<void> => {
+    await db.author.delete({
+        where:{
+            id
+        }
+    })
 }
